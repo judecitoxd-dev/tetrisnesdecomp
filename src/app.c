a288fa61ecdbc80a1c07602ff40a2673a1116457
judecitoxd-dev <judecitoxd@gmail.com>
2026-08-02 16:36:30 -0500

    Place score overlay at a complete C file boundary

--- src/app.c
+++ src/app.c
@@ -1,10 +1,13 @@
 #include "app_v05_00.inc"
+
+static void tetris_present_with_score_overlay(
+    SDL_Renderer *renderer, SDL_Texture *font, AppScreen screen,
+    const TetrisGame *game, const AppResultState *result,
+    const TetrisHighScores *scores);
+
 #include "app_v05_01.inc"
 #include "app_v05_02.inc"
 #include "app_v05_03.inc"
-#include "app_score_overlay.inc"
-#define SDL_RenderPresent(renderer) \
-    tetris_present_with_score_overlay((renderer), font, screen, game, result, scores)
 #include "app_v05_04.inc"
-#undef SDL_RenderPresent
 #include "app_v05_05.inc"
+#include "app_score_overlay.inc"
