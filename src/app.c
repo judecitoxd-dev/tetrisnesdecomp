50643df1853983ef8ca536cc1ee51ae9858f8127
judecitoxd-dev <judecitoxd@gmail.com>
2026-08-02 16:38:33 -0500

    Load the overlay declaration before fragmented app code

--- src/app.c
+++ src/app.c
@@ -1,10 +1,5 @@
+#include "app_score_overlay.h"
 #include "app_v05_00.inc"
-
-static void tetris_present_with_score_overlay(
-    SDL_Renderer *renderer, SDL_Texture *font, AppScreen screen,
-    const TetrisGame *game, const AppResultState *result,
-    const TetrisHighScores *scores);
-
 #include "app_v05_01.inc"
 #include "app_v05_02.inc"
 #include "app_v05_03.inc"
